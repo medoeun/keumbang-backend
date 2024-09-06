0d5f61188dc5f3d42b3007abb36686a8099d4d04
Doeun <id5eun30@gmail.com>
2024-09-06 20:29:44 +0900

    init: 디렉토리 구조 설정 및 기타 초기 세팅

---
+++ auth-server/src/main/java/org/keumbang/AuthServerApplication.java
@@ -0,0 +1,15 @@
+package org.keumbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+}
