9697aedb83f926cd2e6201cd52c130744d9379d2
Doeun <id5eun30@gmail.com>
2024-09-11 19:10:03 +0900

    feat: RefreshToken 엔티티 생성 (#7)

---
+++ auth-server/src/main/java/org/keumbang/entity/RefreshToken.java
@@ -0,0 +1,35 @@
+package org.keumbang.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class RefreshToken {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String token;
+
+	@OneToOne
+	private User user;  // Refresh Token과 사용자 매핑
+
+	@Column(nullable = false)
+	private LocalDateTime expiryDate;
+}
